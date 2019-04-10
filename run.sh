b53acb1828f345e0b719e4fd4c580012b16415c8
Tyler Hunt <thunt@cs.utexas.edu>
2019-04-10 14:07:40 -0500

    run.sh is dead
    run_lgm is the cannonical way to run tests from now on
    
    Signed-off-by: Tyler Hunt <thunt@cs.utexas.edu>

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir=$(dirname $0)
-
-guest_lib=$dir/libguestlib.so
-shim=$dir/guestshim.so
-
-echo HCC_LAZYINIT=1 LD_PRELOAD="$shim:$guest_lib" AVA_LOCAL=1 "$@"
-HCC_LAZYINIT=1 LD_PRELOAD="$shim:$guest_lib" AVA_LOCAL=1 exec "$@"
