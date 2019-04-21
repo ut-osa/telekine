c92902437d5a17a13a026f80edd42cf64f3f6f0a
Zhipeng Jia <zjia@cs.utexas.edu>
2019-04-21 03:47:00 -0500

    optimize crypto for 1MB data size

--- crypto/common.h
+++ crypto/common.h
@@ -7,7 +7,7 @@
 #define AES_ROUNDKEYLEN 240u
 #define AES_BLOCKLEN 16u
 #define AES_MACLEN 12u
-#define AES_GCM_STEP 256u
+#define AES_GCM_STEP 64u
 
 constexpr int kBaseThreadBits = 8;
 constexpr int kBaseThreadNum  = 1 << kBaseThreadBits;
