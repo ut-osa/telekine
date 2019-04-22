ea0bfa135608c905d5c2f2cfbd78ef106389e428
Zhipeng Jia <zjia@cs.utexas.edu>
2019-04-21 22:10:52 -0500

    copy bench

--- copy2.cpp
+++ copy2.cpp
@@ -24,68 +24,64 @@ uint64_t get_microseconds_since_epoch() {
     return result;
 }
 
-__global__ void inc_kernel(uint8_t* ptr, int N, int r)
+__global__ void inc_kernel(float* ptr, int N, int r, float delta)
 {
     int tid = hipThreadIdx_x + hipBlockIdx_x * hipBlockDim_x;
     if (tid >= N) return;
     for (int i = 0; i < r; i++) {
-        ptr[tid]++;
+        ptr[tid] += delta;
     }
 }
 
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s n_round\n", argv[0]);
+        exit(1);
+    }
+
+    int n_round = atoi(argv[1]);
+
     int deviceID = 0;
     CHECK(hipSetDevice(deviceID));
+
+    float delta = (float)rand() / RAND_MAX;
+    int input_size = 1024 * 1024 * 4;
+    size_t n_bytes = input_size * sizeof(float); // 16MB
+    float* A_h = new float[input_size];
+    for (int i = 0; i < input_size; i++) {
+        A_h[i] = (float)rand() / RAND_MAX;
+    }
+    float* A_d;
+    CHECK(hipMalloc(&A_d, n_bytes));
 
     hipStream_t stream;
     CHECK(hipStreamCreate(&stream));
 
-    size_t Nbytes = 16 * 1024 * 1024;
+    uint64_t start_time, current_time;
+    uint64_t threshold = 1000000; // 1s
 
-    uint8_t* A_h = new uint8_t[Nbytes];
-    uint8_t* C_h = new uint8_t[Nbytes];
-
-    for (int i = 0; i < Nbytes; i++) {
-        A_h[i] = rand() & 255;
-    }
-
-    uint8_t* A_d;
-    CHECK(hipMalloc(&A_d, Nbytes));
-    uint8_t* C_d;
-    CHECK(hipMalloc(&C_d, Nbytes));
-
-    uint64_t start_time, elapsed_time;
-
-    for (int r = 0; r < 10; r++) {
-
-        start_time = get_microseconds_since_epoch();
-        CHECK(hipMemcpyAsync(A_d, A_h, Nbytes, hipMemcpyHostToDevice, stream));
+    start_time = get_microseconds_since_epoch();
+    int last_r = -1;
+    int r = 0;
+    while (true) {
+        CHECK(hipMemcpyAsync(A_d, A_h, n_bytes, hipMemcpyHostToDevice, stream));
+        int num_thread = 256;
+        int num_block = (input_size - 1) / num_thread + 1;
+        hipLaunchKernelGGL(
+            HIP_KERNEL_NAME(inc_kernel), num_block, num_thread, 0, stream,
+            A_d, input_size, n_round, delta);
+        CHECK(hipMemcpyAsync(A_h, A_d, n_bytes, hipMemcpyDeviceToHost, stream));
         CHECK(hipStreamSynchronize(stream));
-        elapsed_time = get_microseconds_since_epoch() - start_time;
-        fprintf(stderr, "HtD memcpy: %.3f ms\n", static_cast<double>(elapsed_time) / 1000.0);
-
-        start_time = get_microseconds_since_epoch();
-        CHECK(hipMemcpyAsync(C_d, A_d, Nbytes, hipMemcpyDeviceToDevice, stream));
-        CHECK(hipStreamSynchronize(stream));
-        elapsed_time = get_microseconds_since_epoch() - start_time;
-        fprintf(stderr, "DtD memcpy: %.3f ms\n", static_cast<double>(elapsed_time) / 1000.0);
-
-        start_time = get_microseconds_since_epoch();
-        CHECK(hipMemcpyAsync(C_h, C_d, Nbytes, hipMemcpyDeviceToHost, stream));
-        CHECK(hipStreamSynchronize(stream));
-        elapsed_time = get_microseconds_since_epoch() - start_time;
-        fprintf(stderr, "DtH memcpy: %.3f ms\n", static_cast<double>(elapsed_time) / 1000.0);
-
-        for (int i = 0; i < Nbytes; i++) {
-            if (C_h[i] != A_h[i]) {
-                fprintf(stderr, "Wrong value!\n");
-                break;
-            }
+        current_time = get_microseconds_since_epoch();
+        if (current_time - start_time >= threshold) {
+            fprintf(stderr, "%.3f rounds per sec\n",
+                    static_cast<float>(r - last_r) / ((current_time - start_time) / 1000000.0));
+            start_time = current_time;
+            last_r = r;
         }
-
-        fprintf(stderr, "=================\n");
+        r++;
     }
 
     return 0;
